89d5cc8a7446ae55f7fd40006f9b5aff9c5cb06d
sreenu <sreenu.bojjireddy@gmail.com>
2021-05-15 20:18:01 +0530

    player list

--- teams/urls.py
+++ teams/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('',views.teams_list,name='team_list'),
     path('add_player/',views.add_player,name='add_player'),
     path('save_player/',views.save_player,name='save_player'),
+    path('<int:id>/team_players/',views.team_players,name='team_players'),
 
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,28 +1,29 @@
 from django.db import connection
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .forms import PlayersForm
+from .models import Players
 
 
 def teams_list(request):
     cursor = connection.cursor()
     cursor.execute('''select * from teams_team''')
-    teams=cursor.fetchall()
-    return render(request,'teams/teamslist.html/',{'teams':teams})
+    teams = cursor.fetchall()
+    return render(request, 'teams/teamslist.html/', {'teams': teams})
 
 
 def add_player(request):
     cursor = connection.cursor()
     cursor.execute('''select * from teams_team''')
-    team=cursor.fetchall()
+    team = cursor.fetchall()
     form = PlayersForm()
-    return render(request,'teams/addplayer.html',{'form':form,'team':team})
+    return render(request, 'teams/addplayer.html', {'form': form, 'team': team})
 
 
 def save_player(request):
-    if request.method =='POST':
-        form_obj = PlayersForm(request.POST)
+    if request.method == 'POST':
+        form_obj = PlayersForm(request.POST, request.FILES)
         if form_obj.is_valid():
             form_obj.save()
 
@@ -31,5 +32,8 @@ def save_player(request):
     return HttpResponseRedirect('/teams/')
 
 
-
-
+def team_players(request,id):
+    cursor=connection.cursor()
+    cursor.execute('''select * from teams_players where team_name =(select team_name from team_team where id={})'''.format(id))
+    list=cursor.fetchall()
+    return render(request,'teams/playerlist.html',{'list':list})
